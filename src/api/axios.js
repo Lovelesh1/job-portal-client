d550add0a5870dacbe18b448bba30c4d0bf4d91a
Lovelesh1 <lucky163mehta@gmail.com>
2026-03-06 03:13:52 +0530

    Add /api to backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://job-portal-backend-x29s.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "https://job-portal-backend-x29s.onrender.com/api",
 });
 
 API.interceptors.request.use(
